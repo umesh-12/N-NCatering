e23a73d941c9f5404f32ee48329e5bde7ed09da4
Simanta Sharma Dahal <simantadahal@gmail.com>
2026-06-08 11:14:51 +0545

    home page

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html'
 })
-export class FooterComponent {
+export class FooterComponent implements AfterViewInit, OnDestroy {
 
+  showBackToTop = false;
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', this.toggleBackToTopButton);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('scroll', this.toggleBackToTopButton);
+  }
+
+  toggleBackToTopButton = (): void => {
+    this.showBackToTop = window.scrollY > 300;
+  };
+
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 }
--- src/app/pages/service-page/service-page.component.ts
+++ src/app/pages/service-page/service-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { ServicesComponent } from '../../components/home/services/services.component';
 
 @Component({
   selector: 'app-service-page',
-  imports: [ServicesComponent],
+  imports: [],
   templateUrl: './service-page.component.html'
 })
 export class ServicePageComponent {
