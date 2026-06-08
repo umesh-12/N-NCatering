6fcf0693a06d21166fb66566ce8c2488d507d239
Simanta Sharma Dahal <simantadahal@gmail.com>
2026-06-08 16:17:13 +0545

    home page

--- src/app/pages/menu-single-page/menu-single-page.component.ts
+++ src/app/pages/menu-single-page/menu-single-page.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu-single-page',
-  imports: [],
+  imports: [CommonModule, RouterModule,],
   templateUrl: './menu-single-page.component.html'
 })
 export class MenuSinglePageComponent {
