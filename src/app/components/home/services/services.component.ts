82670eaae0c0828fe06a12b435995f757d58dbaf
Simanta Sharma Dahal <simantadahal@gmail.com>
2026-06-08 16:23:51 +0545

    home page

--- src/app/components/home/services/services.component.ts
+++ src/app/components/home/services/services.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import AOS from 'aos';
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [CommonModule, RouterModule,],
   templateUrl: './services.component.html'
 })
 export class ServicesComponent  implements AfterViewInit {
--- src/app/pages/service-page/service-page.component.ts
+++ src/app/pages/service-page/service-page.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-service-page',
-  imports: [],
+ imports: [CommonModule, RouterModule,],
   templateUrl: './service-page.component.html'
 })
 export class ServicePageComponent {
