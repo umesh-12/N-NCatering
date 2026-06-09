b6fc0f97583d32552e17e72f088e163d9cbaf96c
COMPUTERTRADE\Computer Trade <2@>
2026-06-09 10:33:29 +0545

    changes before

---
+++ src/app/menu-data.ts
@@ -0,0 +1,43 @@
+export const menus = [
+  {
+    id: 1,
+    name: 'N&N Silver',
+    image: 'assets/img/gallery/photo-1.jpg',
+
+    snacks: ['Chicken Chilli', 'French Fries'],
+
+    mainCourse: ['Chicken Curry', 'Fried Rice', 'Dal Fry'],
+
+    salads: ['Green Salad', 'Russian Salad'],
+
+    desserts: ['Ice Cream', 'Gulab Jamun'],
+  },
+
+  {
+    id: 2,
+    name: 'N&N Gold',
+    image: 'assets/img/gallery/photo-2.jpg',
+
+    snacks: ['Chicken Sekuwa', 'Paneer Chilli', 'French Fries'],
+
+    mainCourse: ['Mutton Curry', 'Chicken Curry', 'Fried Rice'],
+
+    salads: ['Fruit Salad', 'Russian Salad'],
+
+    desserts: ['Ice Cream', 'Juju Dhau'],
+  },
+
+  {
+    id: 3,
+    name: 'N&N Diamond',
+    image: 'assets/img/gallery/photo-3.jpg',
+
+    snacks: ['Chicken Wings', 'Fish Fry'],
+
+    mainCourse: ['Mutton Curry', 'Chicken Curry', 'Biryani'],
+
+    salads: ['Green Salad', 'Fruit Salad'],
+
+    desserts: ['Brownie', 'Ice Cream'],
+  },
+];
---
+++ src/app/pages/menu-detail/menu-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-detail',
+  imports: [],
+  templateUrl: './menu-detail.component.html'
+})
+export class MenuDetailComponent {
+
+}
